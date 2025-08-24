166a74cd6bd35be7a553e0631fececa406b57fd1
SARD <sardlord82b.12@gmail.com>
2025-08-23 19:09:48 -0600

    Uploading files of the practice 1

---
+++ P1/Codigos/resultados/Hola_Mundo.java
@@ -0,0 +1,8 @@
+package resultados;
+
+public class Hola_Mundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo 2");
+    }
+}
---
+++ P1/Codigos/resultados/Hola_Mundo_Terminal.java
@@ -0,0 +1,11 @@
+package resultados;
+
+public class Hola_Mundo_Terminal {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo 2");	
+
+	}
+}
